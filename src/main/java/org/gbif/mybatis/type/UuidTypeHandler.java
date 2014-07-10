927d1a081a802f7d750850a73429ba18370a9f62
Markus Döring <mdoering@gbif.org>
2014-07-10 12:24:22 +0200

    Reduce verbose logging, use UrlParser for schemaless urls and make UUID parser fault tolerant

--- src/main/java/org/gbif/mybatis/type/UuidTypeHandler.java
+++ src/main/java/org/gbif/mybatis/type/UuidTypeHandler.java
@@ -1,14 +1,13 @@
 package org.gbif.mybatis.type;
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Types;
-import java.util.UUID;
-
+import com.google.common.base.Strings;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.TypeHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.UUID;
 
 /**
  * Handler for UUID types.
@@ -16,33 +15,43 @@ import org.apache.ibatis.type.TypeHandler;
  * @see UUID
  */
 public class UuidTypeHandler implements TypeHandler<UUID> {
+    private static final Logger LOG = LoggerFactory.getLogger(UuidTypeHandler.class);
 
-  @Override
-  public void setParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
-    if (parameter == null) {
-      ps.setObject(i, null, Types.OTHER);
-    } else {
-      ps.setObject(i, parameter.toString(), Types.OTHER);
+    @Override
+    public void setParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
+        if (parameter == null) {
+            ps.setObject(i, null, Types.OTHER);
+        } else {
+            ps.setObject(i, parameter.toString(), Types.OTHER);
+        }
+
     }
 
-  }
-
-  @Override
-  public UUID getResult(ResultSet rs, String columnName) throws SQLException {
-    if (rs.getString(columnName) != null) {
-      return UUID.fromString(rs.getString(columnName));
+    @Override
+    public UUID getResult(ResultSet rs, String columnName) throws SQLException {
+        return toUUID(rs.getString(columnName));
     }
-    return null;
-  }
 
-  @Override
-  public UUID getResult(ResultSet rs, int columnIndex) throws SQLException {
-    return UUID.fromString(rs.getString(columnIndex));
-  }
+    @Override
+    public UUID getResult(ResultSet rs, int columnIndex) throws SQLException {
+        return toUUID(rs.getString(columnIndex));
+    }
 
-  @Override
-  public UUID getResult(CallableStatement cs, int columnIndex) throws SQLException {
-    return UUID.fromString(cs.getString(columnIndex));
-  }
+    @Override
+    public UUID getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return toUUID(cs.getString(columnIndex));
+    }
+
+    private static UUID toUUID(String val) throws SQLException {
+        if (Strings.isNullOrEmpty(val)) {
+            return null;
+        }
+        try {
+            return UUID.fromString(val);
+        } catch (IllegalArgumentException e) {
+            LOG.warn("Bad UUID found: {}", val);
+        }
+        return null;
+    }
 
 }
